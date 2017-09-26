043cb2f0633a051402701d6299400ca06dc2bc67
Esdras Henrique <esdrasha1@gmail.com>
2017-09-26 11:26:26 -0300

    alteracao na nomeclatura da variavel tamanho para coordenada e adicao de paredes na estrutura da tabela.

--- Beta 1.0/Bloco.lua
+++ Beta 1.0/Bloco.lua
@@ -1,104 +1,104 @@
  local fisica = require("physics")
 fisica.start()
 Bloco={ 
-  Tamanho = 40
+  coordenada = 42
 }
 
 function Bloco:criarBloco()
 math.randomseed(os.time())
 valorBloco = math.random(0,18)
     
-    if(valorBloco == 0) then bloco0 = display.newImage("bloco0.png",Bloco.Tamanho,Bloco.Tamanho) 
+    if(valorBloco == 0) then bloco0 = display.newImage("bloco0.png",Bloco.coordenada,Bloco.coordenada) 
     bloco0.id=0
     fisica.addBody (bloco0,"dynamic")
     return bloco0
     end
-    if(valorBloco == 1) then bloco1 = display.newImage("bloco1.png",Bloco.Tamanho,Bloco.Tamanho) 
+    if(valorBloco == 1) then bloco1 = display.newImage("bloco1.png",Bloco.coordenada,Bloco.coordenada) 
     bloco1.id=1
        fisica.addBody (bloco1,"dynamic")
     return bloco1
     end
-    if(valorBloco == 2) then bloco2 = display.newImage("bloco2.png",Bloco.Tamanho,Bloco.Tamanho) 
+    if(valorBloco == 2) then bloco2 = display.newImage("bloco2.png",Bloco.coordenada,Bloco.coordenada) 
     bloco2.id=2
        fisica.addBody (bloco2,"dynamic")
     return bloco2
     end
-    if(valorBloco == 3) then bloco3 = display.newImage("bloco3.png",Bloco.Tamanho,Bloco.Tamanho) 
+    if(valorBloco == 3) then bloco3 = display.newImage("bloco3.png",Bloco.coordenada,Bloco.coordenada) 
     bloco3.id=3
        fisica.addBody (bloco3,"dynamic")
     return bloco3
     end
-    if(valorBloco == 4) then bloco4 = display.newImage("bloco4.png",Bloco.Tamanho,Bloco.Tamanho) 
+    if(valorBloco == 4) then bloco4 = display.newImage("bloco4.png",Bloco.coordenada,Bloco.coordenada) 
     bloco4.id=4
        fisica.addBody (bloco4,"dynamic")
     return bloco4
     end
-    if(valorBloco == 5) then bloco5 = display.newImage("bloco5.png",Bloco.Tamanho,Bloco.Tamanho) 
+    if(valorBloco == 5) then bloco5 = display.newImage("bloco5.png",Bloco.coordenada,Bloco.coordenada) 
     bloco5.id=5
        fisica.addBody (bloco5,"dynamic")
     return bloco5
     end
-    if(valorBloco == 6) then bloco6 = display.newImage("bloco6.png",Bloco.Tamanho,Bloco.Tamanho) 
+    if(valorBloco == 6) then bloco6 = display.newImage("bloco6.png",Bloco.coordenada,Bloco.coordenada) 
     bloco6.id=6
        fisica.addBody (bloco6,"dynamic")
     return bloco6
     end
-    if(valorBloco == 7) then bloco7 = display.newImage("bloco7.png",Bloco.Tamanho,Bloco.Tamanho) 
+    if(valorBloco == 7) then bloco7 = display.newImage("bloco7.png",Bloco.coordenada,Bloco.coordenada) 
     bloco7.id=7
        fisica.addBody (bloco7,"dynamic")
     return bloco7
     end
-    if(valorBloco == 8) then bloco8 = display.newImage("bloco8.png",Bloco.Tamanho,Bloco.Tamanho) 
+    if(valorBloco == 8) then bloco8 = display.newImage("bloco8.png",Bloco.coordenada,Bloco.coordenada) 
     bloco8id=8
        fisica.addBody (bloco8,"dynamic")
     return bloco8
     end
-    if(valorBloco == 9) then bloco9 = display.newImage("bloco9.png",Bloco.Tamanho,Bloco.Tamanho) 
+    if(valorBloco == 9) then bloco9 = display.newImage("bloco9.png",Bloco.coordenada,Bloco.coordenada) 
     bloco9.id=9
        fisica.addBody (bloco9,"dynamic")
     return bloco9
     end
-    if(valorBloco == 10) then bloco10 = display.newImage("bloco10.png",Bloco.Tamanho,Bloco.Tamanho) 
+    if(valorBloco == 10) then bloco10 = display.newImage("bloco10.png",Bloco.coordenada,Bloco.coordenada) 
     bloco10.id=10
        fisica.addBody (bloco10,"dynamic")
     return bloco10
     end
-    if(valorBloco == 11) then bloco11 = display.newImage("bloco11.png",Bloco.Tamanho,Bloco.Tamanho) 
+    if(valorBloco == 11) then bloco11 = display.newImage("bloco11.png",Bloco.coordenada,Bloco.coordenada) 
     bloco11.id=11
        fisica.addBody (bloco11,"dynamic")
     return bloco11
     end
-    if(valorBloco == 12) then bloco12 = display.newImage("bloco12.png",Bloco.Tamanho,Bloco.Tamanho) 
+    if(valorBloco == 12) then bloco12 = display.newImage("bloco12.png",Bloco.coordenada,Bloco.coordenada) 
     bloco12.id=12
        fisica.addBody (bloco12,"dynamic")
     return bloco12
     end
-    if(valorBloco == 13) then bloco13 = display.newImage("bloco13.png",Bloco.Tamanho,Bloco.Tamanho) 
+    if(valorBloco == 13) then bloco13 = display.newImage("bloco13.png",Bloco.coordenada,Bloco.coordenada) 
     bloco13.id=13
        fisica.addBody (bloco13,"dynamic")
     return bloco13
     end
-    if(valorBloco == 14) then bloco14 = display.newImage("bloco14.png",Bloco.Tamanho,Bloco.Tamanho) 
+    if(valorBloco == 14) then bloco14 = display.newImage("bloco14.png",Bloco.coordenada,Bloco.coordenada) 
     bloco14.id=14
        fisica.addBody (bloco14,"dynamic")
     return bloco14
     end
-   if(valorBloco == 15) then bloco15 = display.newImage("bloco15.png",Bloco.Tamanho,Bloco.Tamanho) 
+   if(valorBloco == 15) then bloco15 = display.newImage("bloco15.png",Bloco.coordenada,Bloco.coordenada) 
     bloco15.id=15
        fisica.addBody (bloco15,"dynamic")
     return bloco15
     end
-      if(valorBloco == 16) then bloco16 = display.newImage("bloco16.png",Bloco.Tamanho,Bloco.Tamanho) 
+      if(valorBloco == 16) then bloco16 = display.newImage("bloco16.png",Bloco.coordenada,Bloco.coordenada) 
     bloco16.id=16
        fisica.addBody (bloco16,"dynamic")
     return bloco16
     end
-    if(valorBloco == 17) then bloco17 = display.newImage("bloco17.png",Bloco.Tamanho,Bloco.Tamanho) 
+    if(valorBloco == 17) then bloco17 = display.newImage("bloco17.png",Bloco.coordenada,Bloco.coordenada) 
     bloco17.id=17
        fisica.addBody (bloco17,"dynamic")
     return bloco17
     end
-    if(valorBloco == 18) then bloco18 = display.newImage("bloco18.png",Bloco.Tamanho,Bloco.Tamanho) 
+    if(valorBloco == 18) then bloco18 = display.newImage("bloco18.png",Bloco.coordenada,Bloco.coordenada) 
     bloco18.id=18
        fisica.addBody (bloco18,"dynamic")
     return bloco18
--- Beta 1.0/TabelaBlocos.lua
+++ Beta 1.0/TabelaBlocos.lua
@@ -2,19 +2,27 @@ local fisica = require("physics")
    fisica.start()
 local alturaDaBase = 500
 local bloco= require("Bloco")
+
+local parede = display.newImage("parede.png")
+ parede.x = display.contentCenterX - 150
+ parede.y = display.contentCenterY + 60
+   fisica.addBody(parede,"static",{friction = 0.3})
+local parede2 = display.newImage("parede.png")
+ parede2.x = display.contentCenterX + 150
+ parede2.y = display.contentCenterY + 60
+   fisica.addBody(parede2,"static",{friction = 0.3})
 local base = display.newImage("base.png")
  base.x = display.contentCenterX
  base.y = display.contentCenterY + alturaDaBase * 0.5
    fisica.addBody(base,"static",{friction = 0.3})
 
 TabelaBlocos = {
-    numeroColunas = 3,
-    numeroLinhas = 3    
+   quantidadeBlocos = 1   
 }
 
 function TabelaBlocos:posicionarBlocos() 
  local blocoPosicionado
-  for linha = 1, TabelaBlocos.numeroLinhas do  
+  for linha = 1, TabelaBlocos.quantidadeBlocos do  
     blocoPosicionado = bloco.criarBloco()
 
   end
